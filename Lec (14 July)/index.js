2a44c232cb074823e1bbca0135c5f71317693ab7
Yajat <yajatdureja@gmail.com>
2025-07-15 14:19:33 +0530

    ok

--- Lec (14 July)/index.js
+++ Lec (14 July)/index.js
@@ -1,26 +1,27 @@
-const { sumOfTwo, subOfTwo, mulOfTwo, divOfTwo } = require('./sumOfTwo.js');
-const readline = require('readline');
+const { sumOfTwo, xy } = require('./calculateTwo/sumOfTwo')
+const mulOfTwo = require('./calculateTwo/mulOfTwo')
+const divOfTwo = require('./calculateTwo/divOfTwo')
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+console.log('SCRIPT JS')
 
-function getUserInput() {
-    rl.question('Enter first number: ', (num1) => {
-        rl.question('Enter second number: ', (num2) => {
-            const a = parseFloat(num1);
-            const b = parseFloat(num2);
-            
-            console.log('\n--- Results ---');
-            console.log('Sum:', sumOfTwo(a, b));
-            console.log('Subtraction:', subOfTwo(a, b));
-            console.log('Multiplication:', mulOfTwo(a, b));
-            console.log('Division:', divOfTwo(a, b));
-            
-            rl.close();
-        });
-    });
-}
+// CALCULATOR
 
-getUserInput();
+// function sumOfTwo(a, b) {
+//     console.log(a + b)
+//     return a + b
+// }
+
+// function mulOfTwo(a, b) {
+//     console.log(a * b)
+//     return a * b
+// }
+
+// function divOfTwo(a, b) {
+//     console.log(a / b)
+//     return a / b
+// }
+
+console.log('XY', xy)
+sumOfTwo(10, 20)
+mulOfTwo(10, 20)
+divOfTwo(10, 20)
